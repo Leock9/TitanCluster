ba0ea43baf1e56f2f06c949322ab2f7fd11546ca
Leonardo Carvalho Khouri <lkhouri@tecnologia-unica.com.br>
2024-05-13 19:04:33 -0300

    eu hein

--- app-lb.tf
+++ app-lb.tf
@@ -17,21 +17,13 @@ resource "aws_lb_target_group" "target-group" {
   vpc_id = data.aws_vpc.default.id
 
   health_check {
-    path     = "/actuator/health"
+    path     = "/health"
     port     = 30007
     matcher  = "200"
     interval = 90
     timeout  = 60
   }
 }
-
-resource "aws_lb_target_group_attachment" "attach" {
-  depends_on       = [aws_eks_cluster.burguer]
-  target_group_arn = aws_lb_target_group.target-group.arn
-  target_id        = data.aws_instance.ec2.id
-  port             = 30007
-}
-
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_alb.alb-cluster.arn
   port              = "80"
