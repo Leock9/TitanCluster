d6f0c407a05e5e52dba76879eefffffbd0909859
Leonardo Carvalho <42394625+Leock9@users.noreply.github.com>
2024-07-16 08:44:24 -0300

    Update target-group.tf

--- target-group.tf
+++ target-group.tf
@@ -1,14 +1,14 @@
 resource "aws_lb_target_group" "tg" {
   name        = "TG-${var.projectName}"
-  port        = 8080
+  port        = 5000
   target_type = "instance"
   protocol    = "HTTP"
 
   vpc_id = data.aws_vpc.default.id
 
   health_check {
-    path    = "/"
-    port    = 8080
+    path    = "/health"
+    port    = 5000
     matcher = "200"
   }
 }
@@ -16,5 +16,5 @@ resource "aws_lb_target_group" "tg" {
 resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = data.aws_instance.ec2.id
-  port             = 8080
+  port             = 5000
 }
